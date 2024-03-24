0695acd2f57842ecdda86d0107b3c2bdfb9e63fb
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2024-03-24 14:06:23 +0530

    for loop

---
+++ Second.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 100;
+    for (int i = 0; i < a; i++)
+    {
+        cout << "Hello " << i << endl;
+    }
+
+    return 0;
+}
